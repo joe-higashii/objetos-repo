e081f0b0ddc21c8141b22b52d3dd4cd0a0701365
Joe <joedsonfera@hotmail.com>
2023-04-17 15:19:13 -0300

    first commit

---
+++ criando-e-manipulando-objetos/intro.js
@@ -0,0 +1,12 @@
+const pessoa = {
+    nome: "Joe",
+    altura: 1.75,
+    peso: 71,
+    idade: 31,
+    signo: "sagitário",
+};
+
+pessoa.nome = "Joedson"
+
+console.log(`Meu nome é ${pessoa.nome}`);
+console.log(`Tenho ${pessoa.idade} anos`);
